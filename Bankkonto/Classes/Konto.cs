67f1b1fc1541cec7fbfb0fcae2fedd794f4499f1
m3xpl4y <m3xpl4y@gmail.com>
2021-04-20 20:21:38 +0200

    Konto klasse bearbeitet

--- Bankkonto/Classes/Konto.cs
+++ Bankkonto/Classes/Konto.cs
@@ -9,6 +9,28 @@ namespace Bankkonto
 {
     class Konto
     {
-        private int kontonummer;
+        public int KontoNummer { get; set; }
+        public double Balance { get; set; }
+        public int Limit { get; set; }
+        public double Fees { get; set; }
+        //CONSTRUCTOR
+        public Konto(int kontoNummer, double balance, int limit, double fees)
+        {
+            KontoNummer = kontoNummer;
+            Balance = balance;
+            Limit = limit;
+            Fees = fees;
+        }
+        //FUNCTIONS
+        public double DepositAmount(double deposit)
+        {
+            Balance = this.Balance + deposit;
+            return Balance;
+        }
+        public double WithdrawAmount(double amount)
+        {
+            Balance = this.Balance - amount;
+            return Balance;
+        }
     }
 }
