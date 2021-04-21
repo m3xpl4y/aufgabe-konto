e42a70cfa80e159f0ca2f2511cdd3086bcd91b3d
m3xpl4y <m3xpl4y@gmail.com>
2021-04-21 22:37:40 +0200

    added AddKonto

---
+++ Bankkonto/Classes/AddKonto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Bankkonto.Classes
+{
+    class AddKonto
+    {
+        public void CreateKreditKonto(Kreditkonto name)
+        {
+            System.Console.WriteLine("Bitte KontoNummer eingeben:" );
+            var KontoNummer = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Bitte Balance eingeben:");
+            var Balance = Convert.ToDouble(Console.ReadLine());
+            System.Console.WriteLine("Bitte Limit eingeben:");
+            var Limit = Convert.ToDouble(Console.ReadLine());
+            System.Console.WriteLine("Bitte Fees eingeben:");
+            var Fees = Convert.ToInt32(Console.ReadLine());
+            var konto = new Kreditkonto(KontoNummer, Balance, Limit, Fees);
+        }
+    }
+}
