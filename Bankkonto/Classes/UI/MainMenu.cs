6a38851aec4740fc37a1ece831f769e5b8b1d31a
m3xpl4y <m3xpl4y@gmail.com>
2021-04-23 12:11:53 +0200

    main menu

--- Bankkonto/Classes/UI/MainMenu.cs
+++ Bankkonto/Classes/UI/MainMenu.cs
@@ -8,6 +8,21 @@ namespace Bankkonto.Classes.UI
 {
     class MainMenu
     {
+        CreateAccounts cr = new CreateAccounts();
+        DepositWithdraw dw = new DepositWithdraw();
+
+
+        String[] mainMenu = { "1. Konto erstellen", "2. Konto abrufen", "3. Beenden"};
         
+        public void Menu()
+        {
+            Console.WriteLine("WILLKOMMEN BEI DER BANK OF 'Wir nehmen dein Geld und du bekommst es nicht mehr'");
+            Console.WriteLine("Wähle wie wir dein Geld nehmen sollen");
+            foreach (string menu in mainMenu)
+            {
+                Console.WriteLine(menu);
+            }
+        }
     }
+
 }
--- Bankkonto/Program.cs
+++ Bankkonto/Program.cs
@@ -11,34 +11,8 @@ namespace Bankkonto
     {
         static void Main(string[] args)
         {
-            CreateAccounts cr = new CreateAccounts();
-            cr.CreateAccountFunction();
-            //AddKreditkonto addkonto = new AddKreditkonto();
-            //addkonto.addKreditKonto();
-            //AddGirokonto girokonto = new AddGirokonto();
-            //girokonto.addGirokonto();
-            
-
-            //Kreditkonto kk = new Kreditkonto(1234649898, 3.75, -200, 0);
-            //Girokonto girokonto1 = new Girokonto(55555666, 2.2, 1500, -250);
-
-
-            //Console.WriteLine("Wählen Sie den Betrag den Sie abheben möchten");
-            //double abhenben = Convert.ToDouble(Console.ReadLine());
-            //kk.WithdrawAmount(abhenben);
-
-            //Console.WriteLine("Ihr Balance: " + girokonto1.Balance + " " + girokonto1.KontoNummer);
-            //Console.WriteLine("Ihr Balance: " + kk.Balance + " " + kk.KontoNummer);
-            //Console.WriteLine("einzahlen");
-            //double einzahlen = Convert.ToDouble(Console.ReadLine());
-            //kk.DepositAmount(einzahlen);
-
-            //Console.WriteLine("Ihr GiroKonto Kontostand beträgt: " + girokonto1.Balance);
-            //Console.WriteLine("Ihr KreditKonto Kontostand beträgt: " + kk.Balance);
-
-            //Console.WriteLine("Ihr GiroKonto Nummer lautet: " + girokonto1.KontoNummer);
-            //Console.WriteLine("Ihr KreditKonto Nummer lautet: " + kk.KontoNummer);
-
+            MainMenu mM = new MainMenu();
+            mM.Menu();
         }
     }
 }
