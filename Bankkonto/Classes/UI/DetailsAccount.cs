2c307e1aaf684a33ff6af170cf6daeaf82785773
m3xpl4y <m3xpl4y@gmail.com>
2021-04-24 21:21:52 +0200

    kontodetails abrufen

---
+++ Bankkonto/Classes/UI/DetailsAccount.cs
@@ -0,0 +1,20 @@
+﻿using Bankkonto.Classes.addKontos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankkonto.Classes.UI
+{
+    class DetailsAccount
+    {
+        AddKonto ad = new AddKonto();
+        
+        public void GetKontoNumber(List<Konto> kontoListe)
+        {
+            Console.WriteLine("Gespeicherte Kontos");
+            ad.GetKontoListe(kontoListe);
+        }
+    }
+}
